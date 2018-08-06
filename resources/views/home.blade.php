b5c16436f5b16f138a2eac2803042782054f74b7
Tony07200 <baptiste.anthony07@gmail.com>
2018-08-06 16:31:27 +0200

    SideNavBar added for each role of  when logged in

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,8 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
+        <div class="col-md-8 mt-3">
+            <div class="card m-5 p-5">
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
