380cd043a7c2041762425b41f811e0ec042aab0a
mariefontanel <marie.fontanel.pro@gmail.com>
2018-08-06 14:02:10 +0200

    fix naming of class for migrations Quality table and QualityType table

--- database/migrations/2018_08_06_081004_create_qualities_table.php
+++ database/migrations/2018_08_06_081004_create_qualities_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFunctionsTable extends Migration
+class Quality extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_08_06_081725_create_quality_types_table.php
+++ database/migrations/2018_08_06_081725_create_quality_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFunctionTypes extends Migration
+class QualityType extends Migration
 {
     /**
      * Run the migrations.
