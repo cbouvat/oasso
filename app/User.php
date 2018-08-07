480b96127a2cf4b66d52e1437314e5917bef6704
Ghis26 <ghislain.chalas@gmail.com>
2018-08-07 13:53:10 +0200

    register.blade.php for client inscription created & create_users_table, RegisterController, User.php Modified

--- app/User.php
+++ app/User.php
@@ -1,23 +1,19 @@
 <?php
-
 namespace App;
-
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
 class User extends Authenticatable
 {
     use Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'lastname', 'firstname', 'email', 'password','gender','birthdate','address_line1','address_line_2','zip_code','city','phone_number_1',
+        'phone_number_2','newspaper','newsletter','gender_joint','firstname_joint','lastname_joint','birthdate_joint','email_joint',
     ];
-
     /**
      * The attributes that should be hidden for arrays.
      *
