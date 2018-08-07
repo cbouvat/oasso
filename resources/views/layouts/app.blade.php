b305f0c817a15020c54bc588c69a189187e2a15e
damien <dubois.damien508@gmail.com>
2018-08-07 12:12:58 +0200

    feature search_bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,12 @@
 
 <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#"></a>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+
+    <form class="w-100" action={{route('search')}} method="get">
+        <input class="form-control form-control-dark w-100" type="search" placeholder="Search" aria-label="Search"
+               name="q">
+    </form>
+
     <ul class="navbar-nav px-3">
         <li class="nav-item text-nowrap">
             <a class="nav-link" href="#">Sign out</a>
--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,11 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/app');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/search','SearchController@search')->name('search');
