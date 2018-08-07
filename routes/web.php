e7dd20934175cd5e92a3e1738e89646af915cbc6
anzorissaev <e.issaev@gmail.com>
2018-08-07 16:40:07 +0200

    Merge remote-tracking branch 'origin/develop' into feature/UserInformationsPage
    # Conflicts:
    #	app/Http/Controllers/UserController.php
    #	routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,6 @@ Route::get('/search','SearchController@search')->name('search');
 
 
 Route::prefix('admin')->group(function () {
-    Route::get('/users-list', 'UserController@index')->name('admin.users.list');
     Route::get('/{user}/soft-delete', 'UserController@softDelete')->name('admin.users.softdelete');
     Route::get('/{user}/before-delete', 'UserController@beforeDelete')->name('admin.users.beforedelete');
 });
