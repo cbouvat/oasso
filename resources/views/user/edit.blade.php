60d62b54a6423ba2f93cac0aaa01ce197c98374c
anzorissaev <e.issaev@gmail.com>
2018-08-08 17:01:52 +0200

    large btn in from-

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -10,109 +10,101 @@
 
                     {{--@dd($user)--}}
 
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+
                     <div class="card bg-light m-5 pb-5 pl-5 pr-5 pt-3">
 
                         <div class="card-header mt-1 mb-5 font-weight-bold"><h4>Mes Infos</h4></div>
 
                         <div class="form-group">
-                            <label for="FormControlSelect1">Civilité</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
-
-                                <option selected disabled>
-
-                                    @if($user->gender == 0)
-                                        Monsieur
-                                    @elseif($user->gender == 1)
-                                        Madame
-                                    @else
-                                        Selectioner votre sexe
-                                    @endif</option>
-                                <option>Monsieur</option>
-                                <option>Madame</option>
-
-                            </select>
+                            <label for="gender">Civilité</label>
+                                <select class="form-control" name="gender" id="gender">
+                                    <option value="1" @if($user->gender == 0) selected @endif>{{ __('Mr') }}</option>
+                                    <option value="2" @if($user->gender == 1) selected @endif>{{ __('Ms') }}</option>
+                                </select>
                         </div>
 
 
                         <div class="form-group">
-                            <label for="InputFirstname">Prénom</label>
-                            <input type="text" class="form-control" id="InputFirstname" aria-describedby="emailHelp"
-                                   value="{{ $user->firstname }}">
+                            <label for="firstname">Prénom</label>
+                            <input type="text" name="firstname" class="form-control" id="firstname" value="{{ $user->firstname }}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="InputLastname">Nom</label>
-                            <input type="text" class="form-control" id="InputLastname" value="{{ $user->lastname }}">
+                            <label for="lastname">Nom</label>
+                            <input type="text" name="lastname" class="form-control" id="lastname" value="{{ $user->lastname }}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="InputEmail">Adresse Email</label>
-                            <input type="email" class="form-control" id="InputEmail" value="{{ $user->email }}">
+                            <label for="email">Adresse Email</label>
+                            <input type="email" name="email" class="form-control" id="email" value="{{ $user->email }}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="inputBirthdate">Date de naissance</label>
-                            <input type="date" class="form-control" id="inputBirthdate" value="{{ $user->birthdate }}">
+                            <label for="birthdate">Date de naissance</label>
+                            <input type="date" name="birthdate" class="form-control" id="birthdate" value="{{ $user->birthdate }}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="InputAddress">Adresse</label>
-                            <input type="text" class="form-control" id="InputAddress" value="{{ $user->address_line1}}">
+                            <label for="address_line1">Adresse</label>
+                            <input type="text" name="address_line1" class="form-control" id="address_line1" value="{{ $user->address_line1}}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="InputAddress">Complément d'adresse</label>
-                            <input type="text" class="form-control" id="InputAddress" value="{{ $user->address_line2}}">
+                            <label for="address_line2">Complément d'adresse</label>
+                            <input type="text" name="address_line2" class="form-control" id="address_line2" value="{{ $user->address_line2}}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="inputCity">Ville</label>
-                            <input type="text" class="form-control" id="inputCity" value="{{ $user->city}}">
+                            <label for="city">Ville</label>
+                            <input type="text" name="city" class="form-control" id="city" value="{{ $user->city}}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="inputZip_code">Code Postal</label>
-                            <input type="text" class="form-control" id="inputZip_code" value="{{ $user->zipcode}}">
+                            <label for="zipcode">Code Postal</label>
+                            <input type="text" name="zipcode" class="form-control" id="zipcode" value="{{ $user->zipcode}}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="inputPhone_number">Téléphone 1</label>
-                            <input type="text" class="form-control" id="inputPhone_number"
+                            <label for="phone_number_1">Téléphone 1</label>
+                            <input type="text" name="phone_number_1" class="form-control" id="phone_number_1"
                                    value="{{ $user->phone_number_1}}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="inputPhone_number">Téléphone 2</label>
-                            <input type="text" class="form-control" id="inputPhone_number"
+                            <label for="phone_number_2">Téléphone 2</label>
+                            <input type="text" name="phone_number_2" class="form-control" id="phone_number_2"
                                    value="{{ $user->phone_number_2}}">
                         </div>
 
 
                         <div class="form-check mt-2">
                             <div class="form-group form-check col-6">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Newsletter</label>
+                                <input type="checkbox" name="newsletter" class="form-check-input" id="newsletter">
+                                <label class="form-check-label" for="newsletter">Newsletter</label>
                             </div>
 
 
                             <div class="form-group form-check col-6">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Journal papier</label>
+                                <input type="checkbox" name="newspaper" class="form-check-input" id="newspaper">
+                                <label class="form-check-label" for="newspaper">Journal papier</label>
                             </div>
 
                         </div>
 
                         <div class="form-group mt-3">
-                            <input type="submit" value="Modifier" class="btn btn-primary btn-lg btn-block">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block">{{ __('Edit') }}</button>
                         </div>
 
                     </div>
@@ -122,58 +114,43 @@
                         <div class="card-header mt-1 mb-5 font-weight-bold"><h4>Infos Conjoint</h4></div>
 
                         <div class="form-group">
-                            <label for="FormControlSelect1">Civilité conjoint</label>
-                            <select class="form-control" id="exampleFormControlSelect1">
-
-
-                                <option selected disabled>
-
-                                    @if($user->gender_joint == 0)
-                                        Monsieur
-                                    @elseif($user->gender_joint == 1)
-                                        Madame
-                                    @else
-                                        Selectioner votre sexe
-                                    @endif</option>
-
-
-                                <option>Monsieur</option>
-                                <option>Madame</option>
-
-
+                            <label for="gender_joint">Civilité conjoint</label>
+                            <select class="form-control" name="gender_joint" id="gender_joint">
+                                <option value="0" @if($user->gender_joint == 0) selected @endif>{{ __('Mr') }}</option>
+                                <option value="1" @if($user->gender_joint == 1) selected @endif>{{ __('Ms') }}</option>
                             </select>
                         </div>
 
                         <div class="form-group">
-                            <label for="InputLastnamePartner">Nom</label>
-                            <input type="text" class="form-control" id="InputLastnamePartner"
+                            <label for="lastname_joint">Nom</label>
+                            <input type="text" name="" class="form-control" id="lastname_joint"
                                    value="{{ $user->lastname_joint }}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="InputFirstnamePartner">Prénom</label>
-                            <input type="text" class="form-control" id="InputFirstnamePartner"
+                            <label for="firstname_joint">Prénom</label>
+                            <input type="text" name="" class="form-control" id="firstname_joint"
                                    value="{{ $user->firstname_joint }}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="inputBirthdatePartner">Date de naissance</label>
-                            <input type="date" class="form-control" id="inputBirthdatePartner"
+                            <label for="birthdate_joint">Date de naissance</label>
+                            <input type="date" name="birthdate_joint" class="form-control" id="birthdate_joint"
                                    value="{{ $user->birthdate_joint }}">
                         </div>
 
 
                         <div class="form-group">
-                            <label for="InputEmailPartner">Email conjoint</label>
-                            <input type="email" class="form-control" id="InputEmailPartner"
+                            <label for="email_joint">Email conjoint</label>
+                            <input type="email_joint" class="form-control" id="email_joint"
                                    value="{{ $user->email_joint}}">
                         </div>
 
 
-                        <div class="form-group mt-3 mb-0">
-                            <input type="submit" value="Modifier" class="btn btn-primary btn-lg btn-block">
+                        <div class="form-group mt-3">
+                            <button type="submit" class="btn btn-primary btn-lg btn-block">{{ __('Edit') }}</button>
                         </div>
                     </div>
 
