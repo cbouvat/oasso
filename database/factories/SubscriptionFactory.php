297f7c1ed385d204001eb09b1f81d6df9de64e19
Raziel-777 <thibault.dams@gmail.com>
2018-08-08 12:04:17 +0200

    subscription_year -> subscriptipon_date

--- database/factories/SubscriptionFactory.php
+++ database/factories/SubscriptionFactory.php
@@ -8,7 +8,7 @@ $factory->define(App\Subscription::class, function (Faker $faker) {
         'opt_out_mail' =>$faker->boolean,
         'user_id' =>$faker->randomNumber(),
         'subscription_type_id' =>$faker->numberBetween(1, 4),
-        'subscription_year' => '2018-01-01'
+        'subscription_date' => '2018-01-01'
 
     ];
 });
