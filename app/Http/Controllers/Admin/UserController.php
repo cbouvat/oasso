2acca0811300069b40e09958c5097f67902bc0d7
mariefontanel <marie.fontanel.pro@gmail.com>
2018-08-09 11:32:34 +0200

    update validation rules with regex

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -47,19 +47,43 @@ class UserController extends Controller
     {
         $this->validate($request,[
             'gender' => 'required|integer',
-            'lastname' => 'required|string|max:45',
-            'firstname' => 'required|string|max:45',
-            'birthdate' => 'required|date',
+            'lastname' => array(
+                'required',
+                'string',
+                'max:45',
+                'regex:/^([a-zA-Z]{2,30}\s*)+/'),
+            'firstname' => array(
+                'required',
+                'string',
+                'max:45',
+                'regex:/^([a-zA-Z]{2,30}\s*)+/'),
+            'birthdate' => 'required|date|before:today-13years|after:today-120years',
             'password' => 'string|min:6|max:191',
-            'address_line1' => 'required|string|max:100',
-            'address_line2' => 'string|max:100|nullable',
-            'zipcode' => 'required|string|max:20',
-            'city' => 'required|string|max:45',
+            'address_line1' => 'required|string|max:32',
+            'address_line2' => 'string|max:32|nullable',
+            'zipcode' => array(
+                'required',
+                'string',
+                'max:20',
+                'regex:/^\d{5}(?:[-\s]\d{4})?$/'),
+            'city' => array(
+                'required',
+                'string',
+                'max:45',
+                'regex:/^([a-zA-Z]{2,})/'),
             'email' => 'email|unique:users|nullable',
             'gender_joint' => 'nullable|integer',
-            'lastname_joint' => 'string|max:45|nullable',
-            'firstname_joint' => 'string|max:45|nullable',
-            'birthdate_joint' => 'date|nullable',
+            'lastname_joint' => array(
+                'string',
+                'max:45',
+                'regex:/^([a-zA-Z]{2,30}\s*)+/',
+                'nullable'),
+            'firstname_joint' => array(
+                'string',
+                'max:45',
+                'regex:/^([a-zA-Z]{2,30}\s*)+/',
+                'nullable'),
+            'birthdate_joint'=> 'date|before:today-13years|after:today-120years|nullable',
             'email_joint' => 'email|max:45|nullable',
             'phone_number_1' => 'string|max:20|nullable',
             'phone_number_2' => 'string|max:20|nullable',
@@ -72,10 +96,37 @@ class UserController extends Controller
             'comment' => 'string|nullable',
             'alert' => 'integer|nullable',
         ]);
-        $password = str_random(40);
 
+        $password = str_random(40);
         $request['password'] = $password;
-        User::create($request->all());
+
+        User::create([
+            'gender' => $request['gender'],
+            'lastname' => $request['lastname'],
+            'firstname' => $request['firstname'],
+            'birthdate' => $request['birthdate'],
+            'password' => $request['password'],
+            'address_line1' => $request['address_line1'],
+            'address_line2' => $request['address_line2'],
+            'zipcode' => $request['zipcode'],
+            'city' => $request['city'],
+            'email' => $request['email'],
+            'gender_joint' => $request['gender_joint'],
+            'lastname_joint' => $request['lastname_joint'],
+            'firstname_joint' => $request['firstname_joint'],
+            'birthdate_joint' => $request['birthdate_joint'],
+            'email_joint' => $request['email_joint'],
+            'phone_number_1' => $request['phone_number_1'],
+            'phone_number_2' => $request['phone_number_2'],
+            'volonteer' => $request['volonteer'],
+            'details_volonteer' => $request['details_volonteer'],
+            'delivery' => $request['delivery'],
+            'newspaper' => $request['newspaper'],
+            'newsletter' => $request['newsletter'],
+            'mailing' => $request['mailing'],
+            'comment' => $request['comment'],
+            'alert' => $request['alert'],
+        ]);
 
         return redirect()->route('admin.users.list');
     }
