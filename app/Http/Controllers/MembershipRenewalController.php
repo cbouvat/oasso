17e7841472b3fb4db28150488c2ebf40734167d2
Tabardelli <remi.tabardel@free.fr>
2018-08-10 12:17:23 +0200

    replace user id hard coded by Auth::id()

--- app/Http/Controllers/MembershipRenewalController.php
+++ app/Http/Controllers/MembershipRenewalController.php
@@ -13,7 +13,7 @@ class membershipRenewalController extends Controller
 {
     public function display()
     {
-        $actualSubscription = Subscription::where('user_id', '9')->orderBy('created_at', 'desc')->first();
+        $actualSubscription = Subscription::where('user_id', Auth::id())->orderBy('created_at', 'desc')->first();
         $actualSubscriptionName = SubscriptionType::where('id', $actualSubscription->subscription_type_id)->first();
         $subscriptionTypes = SubscriptionType::all();
 
