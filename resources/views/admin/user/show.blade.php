199236243019d917c5e9d58f913ec6e0717f2be2
noksit <noksit@live.fr>
2018-08-13 09:02:07 +0200

    font-weight-bold for informations list into show

--- resources/views/admin/user/show.blade.php
+++ resources/views/admin/user/show.blade.php
@@ -5,7 +5,8 @@
         <div class="row d-flex justify-content-between mb-3">
             <a href="{{route('admin.subscription.create')}}" class="btn btn-outline-success">Ajouter une Adhésion</a>
             <a href="#" class="btn btn-outline-primary">Modifer</a>
-            <a href="{{route('admin.user.beforedelete', ['user' => $user])}}" class="btn btn-outline-danger">Supprimer</a>
+            <a href="{{route('admin.user.beforedelete', ['user' => $user])}}"
+               class="btn btn-outline-danger">Supprimer</a>
         </div>
 
         <div class="row ">
@@ -16,42 +17,42 @@
                 <table class="table">
                     <tbody>
                     <tr>
-                        <td class="text-right">Prenom</td>
+                        <td class="font-weight-bold text-right">Prenom</td>
                         <td>{{ $user->firstname }}</td>
                     </tr>
                     <tr>
-                        <td class="text-right">Nom</td>
+                        <td class="font-weight-bold text-right">Nom</td>
                         <td>{{ $user->lastname }}</td>
                     </tr>
                     <tr>
-                        <td class="text-right">Email</td>
+                        <td class="font-weight-bold text-right">Email</td>
                         <td>{{ $user->email }}</td>
                     </tr>
                     <tr>
-                        <td class="text-right">Adresse</td>
+                        <td class="font-weight-bold text-right">Adresse</td>
                         <td>{{ $user->address_line1 }} @if($user->addres_line2) <br>{{ $user->addres_line2 }} @endif
                         </td>
                     </tr>
                     <tr>
-                        <td class="text-right">Code Postal</td>
+                        <td class="font-weight-bold text-right">Code Postal</td>
                         <td>{{ $user->zipcode }}</td>
                     </tr>
                     <tr>
-                        <td class="text-right">Ville</td>
+                        <td class="font-weight-bold text-right">Ville</td>
                         <td>{{ $user->city }}</td>
                     </tr>
                     <tr>
-                        <td class="text-right">Telephone</td>
+                        <td class="font-weight-bold text-right">Telephone</td>
                         <td>{{ $user->phone_number_1 }} @if( $user->phone_number_2 )
                                 <br>{{ $user->phone_number_1 }} @endif
                         </td>
                     </tr>
                     <tr>
-                        <td class="text-right">Newsletter</td>
+                        <td class="font-weight-bold text-right">Newsletter</td>
                         <td>{{ $user->newsletter ? 'Oui' : 'Non' }}</td>
                     </tr>
                     <tr>
-                        <td class="text-right">Newspaper</td>
+                        <td class="font-weight-bold text-right">Newspaper</td>
                         <td>{{ $user->newspaper ? 'Oui' : 'Non'}}</td>
                     </tr>
                     </tbody>
@@ -85,8 +86,6 @@
                                     </tr>
                                 @empty
                                     <tr>
-                                        <td>Aucune</td>
-                                        <td>Adhésion</td>
                                     </tr>
 
                                 @endforelse
@@ -117,8 +116,6 @@
                                     </tr>
                                 @empty
                                     <tr>
-                                        <td>Aucun</td>
-                                        <td>Don</td>
                                     </tr>
                                 @endforelse
                             @endif
