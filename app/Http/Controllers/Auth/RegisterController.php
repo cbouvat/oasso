eac59140a1af50c9e23b6bfa188ba0e8a18046ac
Camille Bouvat <cbouvat@sheep5.com>
2018-08-14 00:24:16 +0200

    Fix space in register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -46,7 +46,6 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
-
         return Validator::make($data, [
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|max:191|confirmed',
