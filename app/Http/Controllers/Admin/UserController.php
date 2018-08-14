4cf74961a5e730c3dfc11c7328a1c760eb886686
Camille Bouvat <cbouvat@sheep5.com>
2018-08-14 10:26:12 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -143,7 +143,7 @@ class UserController extends Controller
         $user = User::findOrFail($id);
         $user->delete();
 
-        return redirect()->route('home')->with('message', $user->firstname . ' supprimé !');
+        return redirect()->route('home')->with('message', $user->firstname.' supprimé !');
     }
 
     /**
