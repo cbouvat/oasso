c276f8ce4846611086b8723e2a4f2c783abff204
noksit <noksit@live.fr>
2018-08-14 15:20:02 +0200

    errors pages 404 / 403 / 503 set and work

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.emptyLayout')
+
+@section('content')
+    <div class="text-center">
+        <img src="https://http.cat/503" alt="">
+    </div>
+    <div>
+        <a href="{{route('home')}}" class="btn btn-primary">Retour</a>
+    </div>
+@endsection
