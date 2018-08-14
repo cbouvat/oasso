1ec0ca08dd524639517648b6525737beadd48b15
MelBou <bouzon.melissa@gmail.com>
2018-08-14 17:11:58 +0200

    all diagrams created + array made. VICTORYgit add app/Http/Controllers/Api/StatisticController.php resources/views/admin/statistics/statistics.blade.php !

--- app/Http/Controllers/Api/StatisticController.php
+++ app/Http/Controllers/Api/StatisticController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Subscription;
-use Carbon\Carbon;
 use DB;
 use Illuminate\Http\Request;
 
@@ -12,51 +10,166 @@ class StatisticController extends Controller
 {
     public function index(Request $request)
     {
-        $months = ["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
+        $months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 
         $type = $request->input('type');
         $dateStart = $request->input('date_start');
         $dateEnd = $request->input('date_end');
 
-        //$datas = Subscription::whereBetween('subscription_date', [$dateStart, $dateEnd])
-        //    ->orderBy('subscription_date')
-          //  ->get()
-          //  ->groupBy(function($date){
-         //       return Carbon::parse($date->subscription_date)->format('F');
-           // });
-        //SELECT *, COUNT(*) FROM `subscriptions` WHERE `subscription_date` BETWEEN '1977-01-01' AND '1977-12-31' GROUP BY MONTH(subscription_date)
+        $dataArray = [];
 
-        $datas = DB::table('subscriptions')->select(DB::raw('count(*) as subscription_count'),
-            DB::raw('MONTH(subscription_date) month'))
-            ->whereBetween('subscription_date', [$dateStart, $dateEnd])
-            ->groupBy('month')
-            ->get();
+        switch ($type) {
+            case 'general':
+                $chartTitle = "Nombre d'adhésions";
+                $chartType = "line";
+
+                $datas = DB::table('subscriptions')->select(DB::raw('count(*) as subscription_count'),
+                    DB::raw('MONTH(subscription_date) as month'))
+                    ->whereBetween('subscription_date', [$dateStart, $dateEnd])
+                    ->groupBy('month')
+                    ->get();
+
+                $dataArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+                foreach ($datas as $data) {
+                    $dataArray[$data->month - 1] = $data->subscription_count;
+                };
+
+                $chartData = [
+                    "labels" => $months,
+                    "datasets" => [
+                        [
+                            "label" => [$chartTitle],
+                            "data" => $dataArray,
+                            "borderColor" => [
+                                "rgba(250,0,0,1)"
+                            ],
+                            "backgroundColor" => [
+                                "rgba(0,0,0,0)"
+                            ],
+                            "borderWidth" => 2
+                        ]
+                    ]
+                ];
+                break;
+            case 'subscriptions':
+                $chartType = "pie";
+                $chartTitle = "Adhésion par type";
+
+                $datas = DB::table('subscriptions')->select(DB::raw('count(*) as subscriptionType_count'))
+                    ->whereBetween('subscription_date', [$dateStart, $dateEnd])
+                    ->groupBy('subscription_type_id')
+                    ->get();
+
+                foreach ($datas as $key => $data) {
+                    $dataArray[$key] = $data->subscriptionType_count;
+                }
+
+                $chartData = [
+                    "labels" => [
+                        'Sans emploi',
+                        'Famille',
+                        'Individuel',
+                        'Etudiant'
+                    ],
+                    "datasets" => [
+                        [
+                            "label" => $chartTitle,
+                            "data" => $dataArray,
+                            "backgroundColor" => [
+                                "#FF6384",
+                                "#36A2EB",
+                                "#FFCE56",
+                                "#69D76B"
+                            ],
+                        ]
+                    ]
+                ];
+
+                break;
+            case 'cities':
+                $chartType = "pie";
+                $chartTitle = "Adhérents par villes";
+
+                $cities = [];
+                $datas = DB::table('subscriptions')
+                    ->join('users', 'user_id', '=', 'users.id')
+                    ->whereBetween('subscription_date', [$dateStart, $dateEnd])
+                    ->select(DB::raw('count(*) as subscribersByCity_count'), DB::raw('users.city as city'))
+                    ->groupBy('users.city')
+                    ->get();
+
+                foreach ($datas as $key => $data) {
+                    $dataArray[$key] = $data->subscribersByCity_count;
+                    $cities[$key] = $data->city;
+                }
+
+                $chartData = [
+                    "labels" => $cities,
+                    "datasets" => [
+                        [
+                            "data" => $dataArray,
+                            "backgroundColor" => [
+                                "#FF6384",
+                                "#36A2EB",
+                                "#FFCE56",
+                                "#69D76B"
+                            ],
+                        ]
+                    ]
+                ];
+                break;
+            case 'receipts':
+                $chartType = "doughnut";
+                $chartTitle = "Répartition des recettes";
+
+                /*$datas = DB::table('subscriptions')
+                    ->whereBetween('subscription_date', [$dateStart, $dateEnd])
+                    ->select(DB::raw('sum(amount) as amountSum'))
+                    ->get();*/
+
+                $datasSubs = DB::table('payments')
+                    ->where('payment_type', '=', 'App\Subscription')
+                    ->join('subscriptions', 'payment_id', '=', 'subscriptions.id')
+                    ->select(DB::raw('sum(payments.amount) as amountSum'))
+                    ->whereBetween('subscriptions.subscription_date', [$dateStart, $dateEnd])
+                    ->get();
+
+                $datasGifts = DB::table('payments')
+                    ->where('payment_type', '=', 'App\Gift')
+                    ->join('gifts', 'payment_id', '=', 'gifts.id')
+                    ->select(DB::raw('sum(payments.amount) as amountSum'))
+                    ->whereBetween('gifts.created_at', [$dateStart, $dateEnd])
+                    ->get();
 
 
-        $dataArray = [0,0,0,0,0,0,0,0,0,0,0,0];
-        foreach ($datas as $data){
-            $dataArray[$data->month -1]=$data->subscription_count;
-        };
+                $dataArray = [$datasSubs->get(0)->amountSum, $datasGifts->get(0)->amountSum];
 
+                $chartData = [
+                    "labels" => [
+                        'Adhésions',
+                        'Dons'
+                    ],
+                    "datasets" => [
+                        [
+                            "data" => $dataArray,
+                            "backgroundColor" => [
+                                "#FF6384",
+                                "#36A2EB",
+                                "#FFCE56",
+                                "#69D76B"
+                            ],
+                        ]
+                    ]
+                ];
+                break;
+            default :
+                return response()->json(['message' => 'Not Found !'], 404);
+        }
 
         $json = [
-            "type"=> "line",
-            "data"=> [
-                "labels"=> $months,
-            "datasets"=> [
-                    [
-                    "label"=> ["Nombre adhésions par mois"],
-                    "data"=> $dataArray,
-                    "borderColor"=> [
-                        "rgba(250,0,0,1)"
-                        ],
-                    "backgroundColor"=>[
-                        "rgba(0,0,0,0)"
-                    ],
-                    "borderWidth"=> 2
-                    ]
-                ]
-          ]];
+            "type" => $chartType,
+            "data" => $chartData
+        ];
 
 
         return $json;
--- resources/views/admin/statistics/statistics.blade.php
+++ resources/views/admin/statistics/statistics.blade.php
@@ -1,61 +1,93 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="mt-5 pt-5">
-        <div class="mb-3">
-            <h2>Statistiques</h2>
+    <h1>Statistiques</h1>
+    <form class="form-row" id="formstat">
+        <div class="col-2">
+            <label for="type"> Type </label>
+            <select class="custom-select mr-sm-2" name="type" id="type">
+                <option selected disabled>Choisir...</option>
+                <option value="general">Général</option>
+                <option value="subscriptions">Adhésions</option>
+                <option value="cities">Villes</option>
+                <option value="receipts">Recettes</option>
+            </select>
         </div>
-        <div class="row">
-            <div class="col-12">
-                <form class="form-row" id="formstat">
-                    <div class="col-2">
-                        <label for="type"> Type </label>
-                        <select class="custom-select mr-sm-2" name="type">
-                            <option selected disabled>Choisir...</option>
-                            <option value="general">Général</option>
-                            <option value="subscriptions">Adhésions</option>
-                            <option value="cities">Villes</option>
-                            <option value="receipts">Recettes</option>
-                        </select>
-                    </div>
-                    <div class="col-3">
-                        <label for="daterange1"> Date de début </label>
-                        <input type="date" name="date_start" value="" class="form-control"/>
-                    </div>
-                    <div class="col-3">
-                        <label for="daterange2"> Date de fin </label>
-                        <input type="date" name="date_end" value="" class="form-control" />
-                    </div>
-                    <div class="col-2">
-                        <label for="range"> Visibilité </label>
-                        <select class="custom-select mr-sm-2" name="range">
-                            <option selected disabled>Choisir...</option>
-                            <option value="days">Jour</option>
-                            <option value="months">Mois</option>
-                            <option value="years">Année</option>
-                        </select>
-                    </div>
-                    <div class="col-2 mt-4 pt-1">
-                        <input class="btn btn-primary" id="btnGeneralPersonalized" type="submit" value="Envoyer">
-                    </div>
-                </form>
-            </div>
-            <div class="col-12">
-                <canvas id="myChartGeneral" width="400" height="200"></canvas>
-                @push('scripts')
-                    <script>
-                        $('#formstat').on('submit', function(event) {
-                            event.preventDefault();
-                            var param = $(this).serialize();
+        <div class="col-3">
+            <label for="daterange1"> Date de début </label>
+            <input type="date" name="date_start" value="" class="form-control"/>
+        </div>
+        <div class="col-3">
+            <label for="daterange2"> Date de fin </label>
+            <input type="date" name="date_end" value="" class="form-control"/>
+        </div>
+        <div class="col-2">
+            <label for="range"> Visibilité </label>
+            <select class="custom-select mr-sm-2" name="range">
+                <option selected disabled>Choisir...</option>
+                <option value="days">Jour</option>
+                <option value="months">Mois</option>
+                <option value="years">Année</option>
+            </select>
+        </div>
+        <div class="col-2 mt-4 pt-1">
+            <input class="btn btn-primary" id="btnGeneralPersonalized" type="submit" value="Envoyer">
+        </div>
+    </form>
 
-                            $.getJSON('/api/stat?' + param, function(data) {
-                                var ctx = document.getElementById("myChartGeneral").getContext('2d');
-                                var myLineChart = new Chart(ctx, data);
-                            });
-                        });
-                    </script>
-                @endpush
-            </div>
-        </div>
+    <canvas id="myChartGeneral" width="400" height="200"></canvas>
+    <div id="tabStat">
+
     </div>
+    @push('scripts')
+        <script>
+            $('#formstat').on('submit', function (event) {
+                event.preventDefault();
+                var param = $(this).serialize();
+                var selectType = $("#type").val();
+
+                $.getJSON('/api/stat?' + param, function (data) {
+                    var ctx = document.getElementById("myChartGeneral").getContext('2d');
+                    var myLineChart = new Chart(ctx, data);
+
+                    if (selectType==="cities") {
+
+                        var sumSubscribers = 0;
+                        for (var i = 0; i < data.data.labels.length; i++) {
+                            sumSubscribers += data.data.datasets[0].data[i];
+                        }
+
+                        function arrayStat() {
+                            var response = "";
+                            for (var j = 0; j < data.data.labels.length; j++) {
+                                var percentage = (data.data.datasets[0].data[j] / sumSubscribers) * 100;
+                                response +=
+                                    '<tr>' +
+                                    '<td>' + data.data.labels[j] + '</td>' +
+                                    '<td>' + percentage + '</td>' +
+                                    '<td>' + data.data.datasets[0].data[j] + '</td>' +
+                                    '</tr>';
+                            }
+                            return response;
+                        }
+
+                        $('#tabStat').html("");
+                        $('#tabStat').append(
+                            '<table class="table">' +
+                            '<thead>' +
+                            '<tr>' +
+                            '<th>Villes</th>' +
+                            '<th>Pourcentages d\'adhérents</th>' +
+                            '<th>Nombres d\'adhérents</th>' +
+                            '</tr>' +
+                            '</thead>' +
+                            '<tbody>' + arrayStat() + '</tbody></table>');
+
+                    }else{
+                        $('#tabStat').html("");
+                    }
+                });
+            });
+        </script>
+    @endpush
 @endsection
