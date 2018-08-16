035f23faaf500e627336584a419757129acacd08
noksit <noksit@live.fr>
2018-08-16 16:20:15 +0200

    fix title+ breadcrumb

--- resources/views/user/history/index.blade.php
+++ resources/views/user/history/index.blade.php
@@ -1,17 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ route('home') }}">Accueil</a></li>
-                <li class="breadcrumb-item">Historique</li>
-            </ol>
-        </nav>
-    </div>
-    <div>
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1>Historique</h1>
     </div>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ route('home') }}">Accueil</a></li>
+            <li class="breadcrumb-item">Historique</li>
+        </ol>
+    </nav>
     <div class="row ">
         <div class="col-6">
 
