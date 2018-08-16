7b40b0b47c24aaec5d647159dcb3e940d407f7c5
Tabardelli <remi.tabardel@free.fr>
2018-08-16 16:20:28 +0200

    pourquoi ça ne commit pas comme je veux?

--- resources/views/user/subscription/create.blade.php
+++ resources/views/user/subscription/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Nouvelle adhésion</h1>
+        <h1>{{ __('New Subscription') }}</h1>
     </div>
 
     <nav aria-label="breadcrumb">
@@ -13,20 +13,22 @@
         </ol>
     </nav>
 
-    <form method="post" action="{{ route('user.subscription.store') }}">
+    <form class="col-4" method="post" action="{{ route('user.subscription.store') }}">
         @csrf
 
         <div class="form-group row">
             <label for="start-date" class="col-sm-4 col-form-label text-md-right">{{ __('Start date') }}</label>
             <div class="col-md-8">
-                <input type="text" readonly disabled class="form-control-plaintext" id="start-date" value="{{ $startDate->format('d/m/Y') }}">
+                <input type="text" readonly disabled class="form-control-plaintext" id="start-date"
+                       value="{{ $startDate->format('d/m/Y') }}">
             </div>
         </div>
 
         <div class="form-group row">
             <label for="end-date" class="col-sm-4 col-form-label text-md-right">{{ __('End date') }}</label>
             <div class="col-md-8">
-                <input type="text" readonly disabled class="form-control-plaintext" id="end-date" value="{{ $endDate->format('d/m/Y') }}">
+                <input type="text" readonly disabled class="form-control-plaintext" id="end-date"
+                       value="{{ $endDate->format('d/m/Y') }}">
             </div>
         </div>
 
@@ -36,7 +38,7 @@
             <div class="col-md-8">
                 <select id="selected-type" name="type" class="custom-select">
                     @foreach($subscriptionTypes as $subscriptionType)
-                        @if ($subscriptionType->name == $actualSubscriptionTypeName)
+                        @if ($subscriptionType->id == $actualSubscriptionTypeId)
                             <option selected value="{{ $subscriptionType->id }}"
                                     data-amount="{{ $subscriptionType->amount }}">
                                 {{ $subscriptionType->name }}
@@ -55,7 +57,8 @@
         <div class="form-group row">
             <label for="subscription-amount" class="col-sm-4 col-form-label text-md-right">{{ __('Price') }}</label>
             <div class="col-md-8">
-                <input type="text" readonly disabled class="form-control-plaintext" id="subscription-amount" value="{{ $endDate->format('d/m/Y') }}">
+                <input type="text" readonly disabled class="form-control-plaintext" id="subscription-amount"
+                       value="{{ $endDate->format('d/m/Y') }}">
             </div>
         </div>
 
@@ -66,15 +69,43 @@
         </div>
     </form>
 
+    <h2>{{ __('Subscriptions History') }}</h2>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="col">{{ __('Start Date') }}</th>
+            <th scope="col">{{ __('End Date') }}</th>
+            <th scope="col">{{ __('Type') }}</th>
+            <th scope="col">{{ __('Amount') }}</th>
+        </tr>
+        </thead>
+        <tbody>
+        @forelse($user->subscriptions as $subscription)
+
+            <tr>
+                <td scope="row">{{$subscription->date_start->format('d/m/Y')}}</td>
+                <td>{{ $subscription->date_end->format('d/m/Y') }}</td>
+                <td>{{ $subscription->type->name}}</td>
+                <td> {{ $subscription->amount}}</td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="4">Vous n'avez réalisé aucun don pour le moment ...</td>
+            </tr>
+        @endforelse
+
+        </tbody>
+    </table>
+
     @push('scripts')
         <script>
-          function selectedtype() {
-            var amount = $('#selected-type option:selected').data('amount');
-            $('#subscription-amount').val(amount + ' €');
-          }
+            function selectedtype() {
+                var amount = $('#selected-type option:selected').data('amount');
+                $('#subscription-amount').val(amount + ' €');
+            }
 
-          $('#selected-type').change(selectedtype);
-          selectedtype();
+            $('#selected-type').change(selectedtype);
+            selectedtype();
         </script>
     @endpush
 
