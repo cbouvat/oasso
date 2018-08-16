f1b5a4f82b29bd588a5c17c3fa3bb3cb66a0c9a4
mariefontanel <marie.fontanel.pro@gmail.com>
2018-08-16 15:28:24 +0200

    add line 9 to manage id in string for the destroy method

--- app/Session.php
+++ app/Session.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Session extends Model
 {
+    public $incrementing = false;
+
     /**
      * The attributes that should be mutated to dates.
      *
