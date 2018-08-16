67f4880dc857d7c21f11f3885e4f92f2fec4fbee
Camille Bouvat <cbouvat@sheep5.com>
2018-08-17 00:56:12 +0200

    Refactoring, remove file of IdeHelpers, fix error blades, fix bug in sidebar, improvement in routes and cleanning delete method on gift

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,8 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <img src="https://http.cat/404" style="max-width: 400px" class="img-fluid rounded mx-auto d-block" alt="">
-    <div class="text-center p-3">
-        <a href="{{ route('home') }}" class="btn btn-outline-secondary">Accueil</a>
-    </div>
+    <img src="https://http.cat/404" style="max-width: 400px" class="img-fluid rounded mx-auto mt-3 d-block" alt="">
 @endsection
