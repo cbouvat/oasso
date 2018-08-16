158b6213ffa8f04809cb9d57199a44e529e3bf23
noksit <noksit@live.fr>
2018-08-16 14:18:56 +0200

    better to see d/m/Y instead of Y-m-d

--- resources/views/admin/subscription/index.blade.php
+++ resources/views/admin/subscription/index.blade.php
@@ -18,11 +18,11 @@
     <table class="table table-hover">
         <thead>
         <tr>
+            <th scope="col">Debut</th>
+            <th scope="col">Fin</th>
             <th scope="col">Adhérant</th>
             <th scope="col">Type d'Adhésion</th>
             <th scope="col">Montant Donné <br> (Montant du Type)</th>
-            <th scope="col">Debut</th>
-            <th scope="col">Fin</th>
             <th scope="col">Methode de paiement</th>
             <th scope="col"></th>
         </tr>
@@ -30,11 +30,11 @@
         <tbody>
         @forelse($subscriptions as $subscription)
             <tr>
+                <td>{{ date('d/m/Y', strtotime($subscription->date_start)) }}</td>
+                <td>{{ date('d/m/Y', strtotime($subscription->date_end)) }}</td>
                 <td scope="row">{{ $subscription->user->firstname }} {{ $subscription->user->lastname }}</td>
                 <td>{{ $subscription->type->name }}</td>
                 <th>{{ $subscription->amount }} € ({{ $subscription->type->amount }} €)</th>
-                <td>{{ $subscription->date_start }}</td>
-                <td>{{ $subscription->date_end }}</td>
                 <td>{{ $subscription->payment ? $subscription->payment->paymentMethod->name : '' }}</td>
                 <td class="text-right">
                     <a href="{{ route('admin.subscription.edit', $subscription) }}"
