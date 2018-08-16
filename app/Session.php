051b7c1a4ca38e9c9e4737b2d61e18594df5b44d
mariefontanel <marie.fontanel.pro@gmail.com>
2018-08-16 11:53:11 +0200

    add relations in models User & Session

--- app/Session.php
+++ app/Session.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Session extends Model
 {
-    //
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
