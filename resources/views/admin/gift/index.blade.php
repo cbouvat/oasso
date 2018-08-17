aa0f9752c997d5943e3d90ed1a0e8eb42cc4773a
mariefontanel <marie.fontanel.pro@gmail.com>
2018-08-17 15:15:09 +0200

    add translations to page admin list gifts

--- resources/views/admin/gift/index.blade.php
+++ resources/views/admin/gift/index.blade.php
@@ -2,23 +2,23 @@
 
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1>Gestion des dons</h1>
+        <h1>{{ __('app.Manage gifts') }}</h1>
     </div>
 
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('home') }}">Accueil</a></li>
-            <li class="breadcrumb-item">Gestion des dons</li>
+            <li class="breadcrumb-item"><a href="{{ route('home') }}">{{ __('app.Home') }}</a></li>
+            <li class="breadcrumb-item">{{ __('app.Manage gifts') }}</li>
         </ol>
     </nav>
 
     <table class="table table-hove">
         <thead>
         <tr>
-            <th scope="col">Donateur</th>
-            <th scope="col">Montant</th>
-            <th scope="col">Date</th>
-            <th scope="col">Methode de paiement</th>
+            <th scope="col">{{ __('app.Donor') }}</th>
+            <th scope="col">{{ __('app.Amount') }}</th>
+            <th scope="col">{{ __('app.Date') }}</th>
+            <th scope="col">{{ __('app.Payment method') }}</th>
             <th scope="col"></th>
         </tr>
         </thead>
@@ -32,14 +32,14 @@
                 <td> {{ $gift->payment ? $gift->payment->paymentMethod->name : '' }}</td>
                 <td class="text-right">
                     <a href="{{route('admin.gift.edit', ['id' => $gift->id])}}"
-                       class="btn btn-sm btn-outline-primary"><span data-feather="edit"></span> Modifier</a>
+                       class="btn btn-sm btn-outline-primary"><span data-feather="edit"></span> {{ __('app.Update') }}</a>
                     <a href="{{route('admin.gift.delete', ['id' => $gift->id])}}"
-                       class="btn btn-sm btn-outline-danger"><span data-feather="trash"></span> Supprimer</a>
+                       class="btn btn-sm btn-outline-danger"><span data-feather="trash"></span> {{ __('app.Remove') }}</a>
                 </td>
             </tr>
         @empty
             <tr>
-                <td colspan="5">Aucun don n'a été realisé</td>
+                <td colspan="5">{{ __('app.No gift') }}</td>
             </tr>
         @endforelse
 
