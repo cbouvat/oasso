10d24e0b23b33ab6ce0cbf902f70cc9382b6bc79
noksit <noksit@live.fr>
2018-08-17 11:32:24 +0200

    refacto admin/gift/create.blade

--- resources/views/admin/gift/create.blade.php
+++ resources/views/admin/gift/create.blade.php
@@ -14,30 +14,35 @@
         </ol>
     </nav>
     <h2>Creer un Don de {{ $user->firstname }} {{ $user->lastname }}</h2>
-    <div class="col-12 col-md-4 offset-md-4 mt-5">
+    <div class="col-md-8 offset-md-2">
         <form action="{{route('admin.user.gift.store', $user)}}" method="post">
             @csrf
-            <label class="col-form-label" for="amount">Montant</label>
-            <div class="input-group">
-                <input type="text" id="amount" name="amount"
-                       class="form-control{{ $errors->has('amount') ? ' is-invalid' : '' }} text-right">
-                <div class="input-group-append">
-                    <span class="input-group-text">€</span>
+            <div class="form-group row">
+                <label class="col-form-label col-md-2" for="amount">Montant</label>
+                <div class="input-group  col-md-4">
+                    <input type="text" id="amount" name="amount"
+                           class="form-control{{ $errors->has('amount') ? ' is-invalid' : '' }} text-right">
+                    <div class="input-group-append">
+                        <span class="input-group-text">€</span>
+                    </div>
                 </div>
-
             </div>
-            <label for="payment_method_id" class="col-form-label">Methode de paiement</label>
-            <div class="input-group">
-                <select class="custom-select form-control{{ $errors->has('amount') ? ' is-invalid' : '' }} text-center"
-                        name="payment_method_id">
-                    @foreach($payments_methods as $payment_method)
-                        <option value="{{ $payment_method->id }}">
-                            {{ $payment_method->name}}
-                        </option>
-                    @endforeach
-                </select>
+            <div class="form-group row">
+                <label for="payment_method_id" class="col-form-label col-md-2">Methode de paiement</label>
+                <div class="input-group col-md-4">
+                    <select class="custom-select form-control{{ $errors->has('amount') ? ' is-invalid' : '' }} text-center"
+                            name="payment_method_id">
+                        @foreach($payments_methods as $payment_method)
+                            <option value="{{ $payment_method->id }}">
+                                {{ $payment_method->name}}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
             </div>
-            <input value="Valider" type="submit" class="btn btn-outline-success btn-block pt-2 mt-2">
+            <div class="col-md-8 offset-md-2">
+                <input value="Valider" type="submit" class="btn btn-success pt-2 mt-2">
+            </div>
         </form>
     </div>
     <div class="mt-5">
