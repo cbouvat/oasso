c61ee8e7952e241d59d88d7cd3163ff84a916a77
mariefontanel <marie.fontanel.pro@gmail.com>
2018-08-17 15:09:53 +0200

    add translations to admin update gift

--- resources/views/admin/gift/edit.blade.php
+++ resources/views/admin/gift/edit.blade.php
@@ -4,16 +4,16 @@
     <div>
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="{{ route('home') }}">Accueil</a></li>
-                <li class="breadcrumb-item"><a href="{{ route('admin.gift.index') }}">Gestion des dons</a></li>
-                <li class="breadcrumb-item">Modification du dons</li>
+                <li class="breadcrumb-item"><a href="{{ route('home') }}">{{ __('app.Home') }}</a></li>
+                <li class="breadcrumb-item"><a href="{{ route('admin.gift.index') }}">{{ __('app.Manage gifts') }}</a></li>
+                <li class="breadcrumb-item">{{ __('app.Update gift') }}</li>
             </ol>
         </nav>
     </div>
     <div class="row">
         <div class="col-12">
             <div>
-                <h1>Modification du Don</h1>
+                <h1>{{ __('app.Update gift') }}</h1>
             </div>
             <div class="text-center p-5 border border-success rounded">
                 <h5>Don de {{ $gift->user->firstname }} {{ $gift->user->lastname }}, le {{ $gift->created_at->format('d/m/Y') }}</h5>
@@ -27,7 +27,7 @@
                                 <span class="input-group-text">€</span>
                             </div>
                         </div>
-                        <input type="submit" class="btn btn-primary" value="Modifier">
+                        <input type="submit" class="btn btn-primary" value="{{ __('app.Update') }}">
                     </form>
                 </div>
             </div>
