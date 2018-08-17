73c9beabe29abeb197f1e4f8e45a7a66357d32ca
mariefontanel <marie.fontanel.pro@gmail.com>
2018-08-17 14:44:15 +0200

    add translations to breadcrumb to page confirm remove a member

--- resources/views/admin/user/beforedelete.blade.php
+++ resources/views/admin/user/beforedelete.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="mt-5 pt-5 text-center">Souhaitez-vous supprimer ce membre ?</h1>
+    <h1 class="mt-5 pt-5 text-center">{{ __('app.Remove this member ?') }}</h1>
     <div class="text-center mt-4 pt-4">
         <a type="button" class="btn btn-danger btn-lg mr-2"
-           href="{{route('admin.user.softdelete', ['user' => $user])}}">Oui</a>
-        <a type="button" class="btn btn-secondary btn-lg ml-2" href="{{ route('user.index') }}">Non</a>
+           href="{{route('admin.user.softdelete', ['user' => $user])}}">{{ __('app.Yes') }}</a>
+        <a type="button" class="btn btn-secondary btn-lg ml-2" href="{{ route('user.index') }}">{{ __('app.No') }}</a>
     </div>
 @endsection
