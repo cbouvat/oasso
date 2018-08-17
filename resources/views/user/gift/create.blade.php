d7c3662ec47be876ed611c95d96c7a7a940f1228
noksit <noksit@live.fr>
2018-08-17 11:19:07 +0200

    refacto user/gift/create.blade

--- resources/views/user/gift/create.blade.php
+++ resources/views/user/gift/create.blade.php
@@ -11,61 +11,48 @@
             <li class="breadcrumb-item">Dons</li>
         </ol>
     </nav>
-    <div>
-        <div class="row">
-            <div class="col-12">
-                <div class="text-center ">
-                    <h5>Participer d'avantage à l'Association {{config('app.name')}}</h5>
-
-                    <div class="col-12 col-md-4 offset-md-4 mt-5">
-                        <form action="{{route('user.gift.store')}}" method="post">
-                            @csrf
-                            <div class="input-group">
-                                <input type="text" name="amount" class="form-control text-right">
-                                <div class="input-group-append">
-                                    <span class="input-group-text">€</span>
-                                </div>
-                            </div>
-                            <div class="text-center">
-                                <input value="Valider" type="submit" class="btn btn-outline-success btn-lg mt-2">
-
-                            </div>
-                        </form>
+    <div class="col-md-8 offset-md-2">
+        <h2>Participer d'avantage à l'Association {{config('app.name')}}</h2>
+        <form action="{{route('user.gift.store')}}" method="post">
+            @csrf
+            <div class="form-group row">
+                <label for="amount" class="col-form-label col-md-2">Montant</label>
+                <div class="input-group col-md-4">
+                    <input id="amount" type="text" name="amount" class="form-control">
+                    <div class="input-group-append">
+                        <span class="input-group-text">€</span>
                     </div>
-
                 </div>
-
             </div>
 
-        </div>
-        <div>
-            <h2>Votre Historique de don</h2>
-        </div>
-        <div class="pt-5">
-            <div class="text-center">
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th scope="col">Montant</th>
-                        <th scope="col">Date</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @forelse($user->gifts as $gift)
-                        <tr>
-                            <th>{{ $gift->amount }} €</th>
-                            <td>{{ $gift->created_at->format('d/m/Y') }}</td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="2">Vous n'avez réalisé aucun don pour le moment ...</td>
-                        </tr>
-                    @endforelse
-                    </tbody>
-                </table>
+            <div class="col-md-8 offset-md-2">
+                <input value="Valider" type="submit" class="btn btn-success mt-2">
             </div>
-
-        </div>
-
+        </form>
+    </div>
+    <div class="pt-5">
+    <h2>Votre Historique de don</h2>
+        <table class="table table-hover">
+            <thead>
+            <tr>
+                <th scope="col">Montant</th>
+                <th scope="col">Date</th>
+                <th scope="col">Methode de paiement</th>
+            </tr>
+            </thead>
+            <tbody>
+            @forelse($user->gifts as $gift)
+                <tr>
+                    <th scope="row">{{ $gift->amount }} €</th>
+                    <td>{{ $gift->created_at->format('d/m/Y') }}</td>
+                    <td>{{ $gift->payment->paymentMethod->name}}</td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="2">Vous n'avez réalisé aucun don pour le moment ...</td>
+                </tr>
+            @endforelse
+            </tbody>
+        </table>
     </div>
 @endsection
