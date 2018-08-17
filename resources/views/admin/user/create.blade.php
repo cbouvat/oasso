ba65654a5f07d2f4d88ec06417e726d493e52c36
mariefontanel <marie.fontanel.pro@gmail.com>
2018-08-17 14:42:20 +0200

    add translations to breadcrumb to page admin create user

--- resources/views/admin/user/create.blade.php
+++ resources/views/admin/user/create.blade.php
@@ -3,13 +3,13 @@
 @section('content')
 
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1>Ajouter un membre</h1>
+        <h1>{{ __('app.Add member') }}</h1>
     </div>
 
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="{{ route('home') }}">Accueil</a></li>
-            <li class="breadcrumb-item">Ajouter un membre</li>
+            <li class="breadcrumb-item"><a href="{{ route('home') }}">{{ __('app.Home') }}</a></li>
+            <li class="breadcrumb-item">{{ __('app.Add member') }}</li>
         </ol>
     </nav>
 
