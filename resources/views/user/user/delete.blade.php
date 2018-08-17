7b022d965e646fc8d9b8d393e5049ffe90a78a87
mariefontanel <marie.fontanel.pro@gmail.com>
2018-08-17 13:49:12 +0200

    add translations for the page User Delete

--- resources/views/user/user/delete.blade.php
+++ resources/views/user/user/delete.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="mt-5 pt-5 text-center">Souhaitez-vous supprimer ce compte ?</h1>
+    <h1 class="mt-5 pt-5 text-center">{{ __('app.Remove this account ?') }}</h1>
     <div class="text-center mt-4 pt-4">
         <a class="btn btn-danger btn-lg"
-           href="{{route('user.delete')}}">Oui</a>
-        <a class="btn btn-secondary btn-lg" href="{{route('user.index', ['user' => $user])}}">Non</a>
+           href="{{route('user.delete')}}">{{ __('app.Yes') }}</a>
+        <a class="btn btn-secondary btn-lg" href="{{route('user.index', ['user' => $user])}}">{{ __('app.No') }}</a>
     </div>
 @endsection
