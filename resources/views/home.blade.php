1ef7f01dd0dbd97f52369f6d84bdf4d42f31d6f0
mariefontanel <marie.fontanel.pro@gmail.com>
2018-08-17 14:14:37 +0200

    add translations to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1>Bienvenue {{ Auth::user()->firstname }}</h1>
+        <h1>{{ __('app.Welcome') }} {{ Auth::user()->firstname }}</h1>
     </div>
 @endsection
