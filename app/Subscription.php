24839b1ecdcc3b279f03a2f62f50bc7d07d3757f
Camille Bouvat <cbouvat@sheep5.com>
2018-10-29 18:32:02 +0100

    Fix bugs and update composer lock

--- app/Subscription.php
+++ app/Subscription.php
@@ -6,11 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscription extends Model
 {
-    protected $dates = [
-        'date_start',
-        'date_end',
-    ];
-
     /**
      * The attributes that are mass assignable.
      *
