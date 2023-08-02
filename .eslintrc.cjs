3b2722a23048b255ff9e3d93039e34cd995f8769
Camille Bouvat <mail@cbouvat.com>
2023-08-02 18:18:04 +0200

    fix: 🐛 added .eslintrc.cjs and updated lint script in package.json

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['plugin:tailwindcss/recommended', 'standard'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['tailwindcss'],
+  rules: {
+    'tailwindcss/no-custom-classname': ['warn', {
+      cssFiles: [
+        'resources/css/app.css'
+      ]
+    }]
+  }
+}
