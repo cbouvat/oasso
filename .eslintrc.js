ec09d246c5296d988e6819aa8a42a9b07b07fb00
Camille Bouvat <mail@cbouvat.com>
2023-08-02 18:13:39 +0200

    fix: 🐛 linter errors and add eslint to package.json scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: ['plugin:tailwindcss/recommended', 'standard'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['tailwindcss'],
-  rules: {
-    'tailwindcss/no-custom-classname': ['warn', {
-      cssFiles: [
-        'resources/css/app.css'
-      ]
-    }]
-  }
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: ['plugin:tailwindcss/recommended', 'standard'],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      },
+    plugins: ['tailwindcss'],
+    rules: {
+        'tailwindcss/no-custom-classname': ['warn', {
+            cssFiles: [
+                'resources/css/app.css'
+            ]
+        }]
+    }
 }
