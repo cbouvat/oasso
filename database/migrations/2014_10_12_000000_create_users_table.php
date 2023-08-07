2a9c51eb46a49e4605960ca2a905657c729df619
Cbibish <sttribet@gmail.com>
2023-08-07 15:05:14 +0200

    feat: :white_check_mark: adding a controller with save method
    allows the code to save a user in its database
    
    user can now be registered and recognised

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
+            $table->string('username', 255);
             $table->string('email')->unique();
             $table->string('firstname', 45);
             $table->string('lastname', 45);
