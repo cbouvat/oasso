5af3435960ebaca53ece4747b0b84b8d09139914
Cbibish <sttribet@gmail.com>
2023-08-09 18:30:29 +0200

    fix: :bug: typo
    correction of typo
    
    none

--- app/Models/User.php
+++ app/Models/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
         'gender',
