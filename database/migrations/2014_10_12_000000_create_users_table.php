66f492253af1e42e7e24f9edab91701b73d8d8fe
Cbibish <sttribet@gmail.com>
2023-08-10 09:58:35 +0200

    fix: :bug: default and nullable values given to all values in user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 255);
+            $table->string('name', 255)->default('');
             $table->string('email')->unique();
             $table->string('firstname', 45)->default('');
             $table->string('lastname', 45)->default('');
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->date('birthdate')->nullable();
             $table->string('address_line1', 255)->default('');
             $table->string('address_line2', 255)->default('');
-            $table->integer('zipcode')->unsigned();
+            $table->integer('zipcode')->unsigned()->nullable();
             $table->string('city', 45)->default('');
             $table->string('phone1', 10)->nullable();
             $table->string('phone2', 10)->nullable();
@@ -33,7 +33,7 @@ return new class extends Migration
             $table->boolean('comment')->nullable();
             $table->boolean('alert')->default(0);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->default('');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
