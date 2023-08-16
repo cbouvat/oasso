d4ab77d2d01b0be140080da9e33b7f4eca7f5ca6
Cbibish <sttribet@gmail.com>
2023-08-16 21:22:33 +0200

    feat: :white_check_mark: basic header structure
    implementation of header template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,15 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Homepage</title>
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-
-<body class="bg-slate-700">
-
-</body>
-
 </html>
