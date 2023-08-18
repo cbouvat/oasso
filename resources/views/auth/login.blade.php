0257353b95e6c44b4b6c8e38a46adaf7010ecc13
Cbibish <sttribet@gmail.com>
2023-08-18 13:12:17 +0200

    feat: changing url in login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
                 <input type="text" id="password" name="password" class="border shadow-inner rounded-lg text-stone-700 w-full focus:shadow-outline">
             </div>
             <div class="w-full flex justify-center">
-                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Se connecter</button>        
+                <a href="http://0.0.0.0/index/userpage"><button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">Se connecter</button></a>        
             </div>
         </div>
     </div>
