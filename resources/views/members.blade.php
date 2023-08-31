3b8a2c15ba073094f3f812b697542ba8ed04cb9e
Cbibish <sttribet@gmail.com>
2023-08-31 17:18:49 +0200

    fix: :construction: quick typo fix

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -5,12 +5,12 @@
     </x-slot>
 
 
-    div class="container">
+<div class="container">
     <table class="table-fixed w-9/12 text-slate-200">
-    <tr><td class="font-bold">Nom</td><td class="font-bold">Email</td><td class="font-extrabold">Mot de passe</td></tr>
-    @foreach($users as $user)
-        <tr><td>{{ $user->name }}</td><td>{{ $user->email }}</td><td>{{ $user->password }}</td></tr>
-    @endforeach
+        <tr><td class="font-bold">Nom</td><td class="font-bold">Email</td><td class="font-extrabold">Mot de passe</td></tr>
+        @foreach($users as $user)
+            <tr><td>{{ $user->name }}</td><td>{{ $user->email }}</td><td>{{ $user->password }}</td></tr>
+        @endforeach
     </table>
 </div>
     
