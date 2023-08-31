bb752391afdefcf91a597333ccb22691fc68e5d5
Cbibish <sttribet@gmail.com>
2023-08-31 16:27:28 +0200

    meaningless changes

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -1,1 +1,8 @@
-{{ $users }}
+<x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+</x-slot>
+<h1 class="text-black">
+        {{ $users }}
+    </h1>
