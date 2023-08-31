274d4b27e107fdbc5006852a6cc3e72a07caad02
Cbibish <sttribet@gmail.com>
2023-08-31 17:16:46 +0200

    feat: :construction: quick addition of header
    addition of layout to member view

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -1,11 +1,12 @@
-<script src="https://cdn.tailwindcss.com"></script>
-<x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-</x-slot>
-<div class="container">
-    <table class="table-fixed w-9/12">
+
+<x-app-layout>
+    <x-slot name="header">
+
+    </x-slot>
+
+
+    div class="container">
+    <table class="table-fixed w-9/12 text-slate-200">
     <tr><td class="font-bold">Nom</td><td class="font-bold">Email</td><td class="font-extrabold">Mot de passe</td></tr>
     @foreach($users as $user)
         <tr><td>{{ $user->name }}</td><td>{{ $user->email }}</td><td>{{ $user->password }}</td></tr>
@@ -13,3 +14,6 @@
     </table>
 </div>
     
+</x-app-layout>
+
+<
