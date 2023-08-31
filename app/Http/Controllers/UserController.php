84d5a56840510dfd2a547bf33714369f7cb66a4e
Cbibish <sttribet@gmail.com>
2023-08-31 14:25:48 +0200

    feat: :construction: Preparations for member page
    Creation of route and controller for members page

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /** 
+     * Returns all existing users
+    */
+        
+    public function showAll() {
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,3 +26,5 @@ Route::middleware([
 });
 
 Route::get('/', HomeController::class)->name('index');
+
+Route::get('/Members', [UserController::class, 'showAll']); 
