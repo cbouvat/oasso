d6f13c2d93860ca1da6f1d613f64c5e1f08a5f60
Cbibish <sttribet@gmail.com>
2023-08-31 17:09:07 +0200

    style: :construction: getting basic presentation needs

--- resources/views/members.blade.php
+++ resources/views/members.blade.php
@@ -1,8 +1,15 @@
+<script src="https://cdn.tailwindcss.com"></script>
 <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
 </x-slot>
-<h1 class="text-black">
-        {{ $users }}
-    </h1>
+<div class="container">
+    <table class="table-fixed w-9/12">
+    <tr><td class="font-bold">Nom</td><td class="font-bold">Email</td><td class="font-extrabold">Mot de passe</td></tr>
+    @foreach($users as $user)
+        <tr><td>{{ $user->name }}</td><td>{{ $user->email }}</td><td>{{ $user->password }}</td></tr>
+    @endforeach
+    </table>
+</div>
+    
