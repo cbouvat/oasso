7db79532df74e7c3bcf1c2c2518b9a5699a2085d
Cbibish <sttribet@gmail.com>
2023-09-01 11:21:30 +0200

    feat: :construction: Addition of search function on the usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,8 +11,19 @@ class UserController extends Controller
      * Returns all existing users
     */
         
-    public function showAll() {
+    public function showAll()
+    {
         $users = User::all();
         return view('members', ['users' => $users]);
+    }
+
+    /**
+     * Searches for a specific user
+     */
+    public function search(Request $request) 
+    {
+        $query = $request->query('namesearch');
+        $result = User::where('name', 'LIKE', '%' . $query . '%')->get();
+        return view('results', ['users' => $result]);
     }
 }
