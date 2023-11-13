5d421997f1faa715c399fdadb8e4f6f6f1b52fd4
Camille Bouvat <mail@cbouvat.com>
2023-11-13 17:38:33 +0100

    feat: 🎨 Indent postcss.config.js, resources/js/app.js, and vite.config.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,6 @@ import './bootstrap'
 
 import Alpine from 'alpinejs'
 import focus from '@alpinejs/focus'
-window.Alpine = Alpine
 
 Alpine.plugin(focus)
-
 Alpine.start()
--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { defineConfig } from 'vite'
+import laravel, { refreshPaths } from 'laravel-vite-plugin'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: [
-                ...refreshPaths,
-                'app/Livewire/**',
-            ],
-        }),
-    ],
-    server: {
-        hmr: {
-            host: 'localhost'
-        }
+  plugins: [
+    laravel({
+      input: [
+        'resources/css/app.css',
+        'resources/js/app.js'
+      ],
+      refresh: [
+        ...refreshPaths,
+        'app/Livewire/**'
+      ]
+    })
+  ],
+  server: {
+    hmr: {
+      host: 'localhost'
     }
-});
+  }
+})
