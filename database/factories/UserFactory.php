054b45bfc377a8271e4f627c3d78cf072c3e96f2
Camille Bouvat <mail@cbouvat.com>
2023-12-18 15:56:33 +0100

    feat: 🗑️ Remove unused code in UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,11 +2,8 @@
 
 namespace Database\Factories;
 
-use App\Models\Team;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
-use Laravel\Jetstream\Features;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -50,26 +47,5 @@ class UserFactory extends Factory
                 'email_verified_at' => null,
             ];
         });
-    }
-
-    /**
-     * Indicate that the user should have a personal team.
-     */
-    public function withPersonalTeam(?callable $callback = null): static
-    {
-        if (! Features::hasTeamFeatures()) {
-            return $this->state([]);
-        }
-
-        return $this->has(
-            Team::factory()
-                ->state(fn (array $attributes, User $user) => [
-                    'name' => $user->name.'\'s Team',
-                    'user_id' => $user->id,
-                    'personal_team' => true,
-                ])
-                ->when(is_callable($callback), $callback),
-            'ownedTeams'
-        );
     }
 }
