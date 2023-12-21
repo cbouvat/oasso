9551744eba3756fe442577ac5f117f4cfec2eeae
Camille Bouvat <mail@cbouvat.com>
2023-12-21 16:46:53 +0100

    feat: 🎨 Laravel Pint

--- database/factories/PaymentFactory.php
+++ database/factories/PaymentFactory.php
@@ -3,8 +3,8 @@
 namespace Database\Factories;
 
 use App\Models\Donation;
-use App\Models\PaymentType;
 use App\Models\Membership;
+use App\Models\PaymentType;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
