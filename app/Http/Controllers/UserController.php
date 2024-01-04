910dd8b78a419b9c3c27779fab8b3cdea9b44398
Cbibish <sttribet@gmail.com>
2024-01-04 16:12:10 +0100

    fix: :bug: typo correction

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -48,7 +48,7 @@ class UserController extends Controller
      */
     public function show(User $user)
     {
-        return view('users.profile', ['users' => $users]);
+        return view('users.profile', ['user' => $user]);
     }
 
     /**
