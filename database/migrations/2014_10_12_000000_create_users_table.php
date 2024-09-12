f2d6b6a919a29827125f6f22ae5b255d03a03588
Romain <damiensx23_3@hotmail.com>
2024-09-12 11:40:59 +0200

    migration users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,13 @@ return new class extends Migration
         Schema::create('users', static function (Blueprint $table): void {
             $table->id();
             $table->enum('gender', ['male', 'female', 'other']);
-            $table->string('last_name', 45);
+            $table->string('last_name', 45)->index();
             $table->string('first_name', 45);
             $table->date('date_of_birth')->nullable();
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('newsletter')->default(0);
-            $table->string('password');
+            $table->string('password', 150);
             $table->rememberToken();
             $table->string('address', 100)->nullable();
             $table->string('address_2', 100)->nullable();
@@ -30,6 +30,11 @@ return new class extends Migration
             $table->string('phone_2', 20)->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
             $table->text('comment')->nullable();
+            $table->string('job', 50)->nullable();
+            $table->string('contact_name')->nullable();
+            $table->string('contact_phone')->nullable();
+            $table->integer('nb_children')->nullable();
+            $table->foreignId('parent_id')->nullable();
             $table->timestamps();
         });
     }
