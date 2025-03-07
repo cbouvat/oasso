a05521126e3d4d52367255d31703b869badfe2db
Camille Bouvat <mail@cbouvat.com>
2025-03-07 17:20:47 +0100

    feat: 🗑️ remove unused default theme configuration from Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
@@ -8,14 +7,5 @@ export default {
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
     plugins: [forms],
 };
